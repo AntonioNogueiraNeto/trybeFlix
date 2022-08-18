ae1f563ed989e6d52f4770e70d2f665214cfae13
Antônio <antonio.nogueiraneto@icloud.com>
2022-08-18 13:14:00 -0300

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Catalog from './components/Catalog';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Catalog />
+    </>
+  );
+}
+
+export default App;
---
+++ src/data/movies.js
@@ -0,0 +1,67 @@
+export const movieList = [
+  {
+    movieName: 'O Poderoso Chefão',
+    releaseYear: 1972,
+    image: 'https://br.web.img3.acsta.net/medias/nmedia/18/90/93/20/20120876.jpg',
+  },
+  {
+    movieName: 'Interstellar',
+    releaseYear: 2014,
+    image: 'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg',
+  },
+  {
+    movieName: 'Rocky Balboa',
+    releaseYear: 2006,
+    image: 'https://br.web.img3.acsta.net/medias/nmedia/18/91/95/32/20171186.jpg',
+  },
+  {
+    movieName: 'Raya',
+    releaseYear: 2021,
+    image: 'https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2021/01/27/rayaandthelastdragonver5xlg.jpg',
+  },
+  {
+    movieName: 'Top Gun: Maverick',
+    releaseYear: 2022,
+    image: 'http://www.cinesystem.com.br/filmes/images/poster/top-gun-maverick.jpg',
+  },
+  {
+    movieName: 'Thor Love and Thunder',
+    releaseYear: 2022,
+    image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/0/08/Thor_Love_and_Thunder_poster.jpg/250px-Thor_Love_and_Thunder_poster.jpg',
+  },
+  {
+    movieName: 'Prey',
+    releaseYear: 2022,
+    image: 'https://i0.wp.com/www.arrobanerd.com.br/wp-content/uploads/2022/06/prey.jpg?fit=1182%2C1478&ssl=1',
+  },
+  {
+    movieName: 'Elvis',
+    releaseYear: 2022,
+    image: 'https://br.web.img2.acsta.net/pictures/22/05/18/09/51/0772429.jpg',
+  },
+  {
+    movieName: 'Jurassic World 3',
+    releaseYear: 2022,
+    image: 'https://br.web.img3.acsta.net/pictures/22/04/15/22/05/1348210.jpg',
+  },
+  {
+    movieName: 'Procurando Nemo',
+    releaseYear: 2003,
+    image: 'https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg',
+  },
+  {
+    movieName: 'Ratatouille',
+    releaseYear: 2007,
+    image: 'https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg',
+  },
+  {
+    movieName: 'Soul',
+    releaseYear: 2020,
+    image: 'https://upload.wikimedia.org/wikipedia/en/3/39/Soul_%282020_film%29_poster.jpg',
+  },
+  {
+    movieName: 'Vida de inseto',
+    releaseYear: 1998,
+    image: 'https://upload.wikimedia.org/wikipedia/en/c/cc/A_Bug%27s_Life.jpg',
+  },
+];
